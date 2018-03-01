9762b97b4f565459f4532b32c4e19aa23a18cc75
Bruno Brito <bruno.debrito@tudelft.nl>
2018-03-01 22:46:27 +0100

    ploting all the agent and gaussian distributions for the predictions

--- social_lstm/social_visualize.py
+++ social_lstm/social_visualize.py
@@ -51,6 +51,9 @@ def plot_trajectories(true_trajs, pred_trajs, obs_length, name):
     traj_data = {}
     pred_data = {}
     obs_data = {}
+    pred_data = []
+    pred_cov_data = []
+    obs_data = []
     obs_samples_lem=0
     # For each frame/each point in all trajectories
     for j in range(maxNumPeds):
@@ -78,10 +81,8 @@ def plot_trajectories(true_trajs, pred_trajs, obs_length, name):
 
                 if (j not in traj_data) and i < obs_length:
                     traj_data[j] = []
-                    pred_data[j] = []
-                    obs_data[j] = []
                 fran=False
-                if j in traj_data:
+                if True:
                     traj_data[j].append(list(true_pos[j, 1:3]))
                     if fran:
                         for index, sample in enumerate(pred_trajs):
@@ -90,10 +91,12 @@ def plot_trajectories(true_trajs, pred_trajs, obs_length, name):
                             else:
                                 obs_data[j].append(list(sample[i][j, 1:3]))
                     else:
-                        if i >= obs_length:
-                            pred_data[j].append(list(pred_trajs[i][j][1:3]))
-                        else:
-                            obs_data[j].append(list(pred_trajs[i][j][1:3]))
+                        if true_pos[j, 0]==1:
+                            if i >= obs_length:
+                                pred_data.append(list(pred_trajs[i][j][1:3]))
+                                pred_cov_data.append(list(pred_trajs[i][j][3:6]))
+                            else:
+                                obs_data.append(list(pred_trajs[i][j][1:3]))
                         # traj_data[j][1].append(pred_pos[j, 1:3])
     true_x=np.zeros(traj_length)
     true_y=np.zeros(traj_length)
@@ -101,40 +104,53 @@ def plot_trajectories(true_trajs, pred_trajs, obs_length, name):
     pred_y=np.zeros(traj_length-obs_length)
     obs_x=np.zeros(obs_length)
     obs_y=np.zeros(obs_length)
+    for j in range(1,len(traj_data)):
+        if j in traj_data:
+            for i in range(len(traj_data[j])):
+                true_x[i] = (1. + traj_data[j][i][0]) * width / 2.
+                true_y[i] = (1. + traj_data[j][i][1]) * height / 2.
+                if i>= obs_length:
+                    pred_x[i-8] = (1. + pred_data[i-8][0]) * width / 2.
+                    pred_y[i-8] = (1. + pred_data[i-8][1]) * height / 2.
+                if i < obs_length:
+                    obs_x[i] = (1. + obs_data[i][0]) * width / 2.
+                    obs_y[i] = (1. + obs_data[i][1]) * height / 2.
 
-    for i in range(traj_length):
-        x=traj_data[1][i]
-        true_x[i] = (1. + traj_data[1][i][0]) * width / 2.
-        true_y[i] = (1. + traj_data[1][i][1]) * height / 2.
-        if i>= obs_length:
-            pred_x[i-8] = (1. + pred_data[1][i-8][0]) * width / 2.
-            pred_y[i-8] = (1. + pred_data[1][i-8][1]) * height / 2.
-        if i < obs_length:
-            obs_x[i] = (1. + obs_data[1][i][0]) * width / 2.
-            obs_y[i] = (1. + obs_data[1][i][1]) * height / 2.
+                label1 = 'Pedestrian observed states'
+                label2 = 'Pedestrian no-observed states'
 
-        label1 = 'Pedestrian observed states'
-        label2 = 'Pedestrian no-observed states'
-
-        limits_x = (1. + max(obs_data[1][:][0])) * height / 2.
-        limits_y = (1. + max(obs_data[1][:][1])) * width / 2.
-
-        label1 = 'Robot observed states'
-        label2 = 'Robot predicted states'
-        label3 = 'Robot real states'
-
-        # Ploting predicted trajectories for time t till t+12
-    ax1.plot(obs_x, obs_y, color='g',linestyle='solid', marker='o', label=label1)
-    #Predicted trajectories
-    ax1.plot(pred_x, pred_y, color='b', linestyle='solid', marker='o', label=label2)
-    # Ploting real/observed trajectory from t till t+8
-    ax1.plot(true_x, true_y, color='b',linestyle='solid', marker='_', label=label3)
-    # ax2.plot(true_x[0:min(obs_length, len(true_x))], true_y[0:min(obs_length,len(true_y))], color=c, linestyle='solid', marker='o', label=label1)
-    ax1.imshow(im)
-    leg1 = ax1.legend(loc='lower right', shadow=True)
-    ax1.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
+                limits_x=pred_x
+                limits_y=pred_y
+                obs_samples_lem = len(limits_x)
+                label1 = 'Robot observed states'
+                label2 = 'Robot predicted states'
+                label3 = 'Robot real states'
+                label4 = 'Other Pedestrians'
+                # Ploting predicted trajectories for time t till t+12
+                x=len(pred_x)
+            if pred_x[0] >0:
+                ax1.plot(obs_x[obs_x !=0], obs_y[obs_y !=0], color='g',linestyle='solid', marker='o', label=label1)
+                #Predicted trajectories
+                ax1.plot(pred_x[pred_x !=0], pred_y[pred_y !=0], color='b', linestyle='solid', marker='o', label=label2)
+                ax1.plot(true_x[true_x !=0], true_y[true_y !=0], color='b', linestyle='solid', marker='_', label=label3)
+                sns.kdeplot(pred_x[pred_x !=0], pred_y[pred_y !=0], shade=True, ax=ax1, shade_lowest=False, cmap="Blues")
+                #sns.kdeplot(limits_x[obs_samples_lem:len(limits_x)], limits_y[obs_samples_lem:len(limits_x)],
+                #            shade=True, ax=ax1, shade_lowest=False)
+            else:
+                # Ploting real/observed trajectory from t till t+8
+                ax1.plot(true_x[true_x !=0], true_y[true_y !=0], color='r',linestyle='solid', marker='o', label=label4)
+            # ax2.plot(true_x[0:min(obs_length, len(true_x))], true_y[0:min(obs_length,len(true_y))], color=c, linestyle='solid', marker='o', label=label1)
+        ax1.imshow(im)
+        leg1 = ax1.legend(loc='lower right', shadow=True)
+        ax1.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
+        true_x = np.zeros(traj_length)
+        true_y = np.zeros(traj_length)
+        pred_x = np.zeros(traj_length - obs_length)
+        pred_y = np.zeros(traj_length - obs_length)
+        obs_x = np.zeros(obs_length)
+        obs_y = np.zeros(obs_length)
     plt.show()
-    plt.close()
+    #plt.close()
 
 
 def main():
@@ -151,7 +167,7 @@ def main():
         if fran:
             plot_trajectories(results[i][0][0], results[i][1:-1], results[i][0][1], name)
         else:                    #header         #first sampled traj     #number obbservations
-            plot_trajectories(results[i][0][0], results[i][1], results[i][0][1], name)
+            plot_trajectories(results[i][0][0], results[i][3], results[i][0][1], name)
 
 
 
